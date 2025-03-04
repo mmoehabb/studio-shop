dea1075f59d93d5cef745d861a99dd839eb4c8c0
M. E. Abdelsalam <mo.ehab150@gmail.com>
2025-03-04 20:58:12 +0200

    added: devtools detector, watermark in photo component, and more site security

--- public/init.js
+++ public/init.js
@@ -3,3 +3,9 @@ const AppState = new StateManager();
 document.addEventListener("DOMContentLoaded", () => {
   AppState.set(SELECTED_NAV_OPTION, 0);
 });
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "F12") {
+    e.preventDefault();
+  }
+});
--- public/util.js
+++ public/util.js
@@ -4,8 +4,16 @@ function switchMode() {
   html.setAttribute("theme", theme !== "dark" ? "dark" : "light")
 }   
 
+function detectDevtools(event) {
+  if (event?.detail.isOpen || devtools.isOpen) {
+    window.location.replace("https://static.planetminecraft.com/files/resource_media/screenshot/1234/Hacker_Detected_3389798.jpg");
+  }
+}
+
 // Get notified when it's opened/closed or orientation changes
-window.addEventListener('devtoolschange', event => {
-    console.log('Is DevTools open:', event.detail.isOpen);
-    console.log('DevTools orientation:', event.detail.orientation);
+window.addEventListener('devtoolschange', (event) => {
+  console.log(event)
+  detectDevtools(event);
 });
+
+detectDevtools();
